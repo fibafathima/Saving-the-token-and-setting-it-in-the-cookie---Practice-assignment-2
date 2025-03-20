4c1f67092c8ceb2647ff162ad27db1b6c21644e1
fiba fathima <fibaaminnu@gmail.com>
2025-03-20 15:16:37 +0530

    completed assignment

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+const jwt = require('jsonwebtoken');
+
+
+
 const encrypt = (payload, secret) => {
+  return jwt.sign(payload,secret,{expiresIn:'1h'})
   // your code here and return token
 };
 
-module.exports = encrypt;
+const verifyToken = (token,secret)=>{
+  try {
+    const decoded = jwt.verify(token,secret)
+    return {valid:true,expired:false,decoded}
+  } catch (error) {
+    return {valid:false,expired:error.name==='TokenExpiredError'}
+    
+  }
+}
+
+const secret = "SH123OKN"
+const payload = {userId:123,role:'admin'};
+
+const token = encrypt(payload,secret)
+console.log("Generated Token:",token);
+
+setTimeout(()=>{
+  const result =verifyToken(token,secret)
+  console.log('Tpken verification:',result)
+},2000)
+
+module.exports = {encrypt,verifyToken};
